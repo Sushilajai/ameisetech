c3e03962b0e5d1d26d892d6fbbec1a2393116504
sushilajai <sushila@ameisetec.com>
2026-02-10 17:12:00 +0530

    Deploy Vue site to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 // // vue.config.js
-// module.exports = {
-//   runtimeCompiler: true,
-//     publicPath: process.env.NODE_ENV === "production" ? "/ameisetech/" : "/",
-
-// };
 module.exports = {
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+  runtimeCompiler: true,
+    publicPath: process.env.NODE_ENV === "production" ? "/ameisetech/" : "/",
+
 };
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// };
